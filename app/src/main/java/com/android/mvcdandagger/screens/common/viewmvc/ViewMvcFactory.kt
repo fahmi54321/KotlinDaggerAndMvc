6d96145ba3b9653c6504a65471ea0bb05c995c14
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-12-09 19:04:15 +0700

    create mvc factory

--- app/src/main/java/com/android/mvcdandagger/screens/common/viewmvc/ViewMvcFactory.kt
+++ app/src/main/java/com/android/mvcdandagger/screens/common/viewmvc/ViewMvcFactory.kt
@@ -2,6 +2,7 @@ package com.android.mvcdandagger.screens.common.viewmvc
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.android.mvcdandagger.screens.questiondetails.QuestionDetailsListViewMvc
 import com.android.mvcdandagger.screens.questionslist.QuestionsListViewMvc
 
 class ViewMvcFactory(
@@ -11,6 +12,10 @@ class ViewMvcFactory(
     //todo 4 (next ActivityCompositionRoot)
     fun newQuestionsListViewMvc(parent: ViewGroup?): QuestionsListViewMvc{
         return QuestionsListViewMvc(layoutInflater, parent)
+    }
+
+    fun newQuestionsDetailsViewMvc(parent: ViewGroup?): QuestionDetailsListViewMvc{
+        return QuestionDetailsListViewMvc(layoutInflater,parent)
     }
 
 }
