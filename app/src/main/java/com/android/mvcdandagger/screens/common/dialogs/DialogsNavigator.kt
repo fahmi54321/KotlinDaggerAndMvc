f82ed5ef8fd2776c813ca81e37a5ccf09d975888
fahmi <fahmiabdulaziz44@gmail.com>
2023-12-31 21:29:27 +0700

    update todo

--- app/src/main/java/com/android/mvcdandagger/screens/common/dialogs/DialogsNavigator.kt
+++ app/src/main/java/com/android/mvcdandagger/screens/common/dialogs/DialogsNavigator.kt
@@ -2,12 +2,10 @@ package com.android.mvcdandagger.screens.common.dialogs
 
 import androidx.fragment.app.FragmentManager
 
-//todo 1
 class DialogsNavigator(
     private val fragmentManager: FragmentManager
 ) {
 
-    //todo 2 (next QuestionsListActivity)
     fun showServerErrorDialogFragment(){
         fragmentManager.beginTransaction()
             .add(ServerErrorDialogFragment.newInstance(), null)
