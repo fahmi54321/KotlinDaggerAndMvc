b5e459282e27ecb2aa0bbb5c9446c0e4340ad07b
fahmi <fahmiabdulaziz44@gmail.com>
2024-01-01 16:36:46 +0700

    update todo

--- app/src/main/java/com/android/mvcdandagger/common/composition/ActivityCompositionRoot.kt
+++ app/src/main/java/com/android/mvcdandagger/common/composition/ActivityCompositionRoot.kt
@@ -1,25 +1,19 @@
 package com.android.mvcdandagger.common.composition
 
-import android.app.Activity
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import com.android.mvcdandagger.screens.common.dialogs.DialogsNavigator
 import com.android.mvcdandagger.screens.common.navigator.ScreenNavigator
-import com.android.mvcdandagger.screens.common.viewmvc.ViewMvcFactory
-import com.android.mvcdandagger.screens.questiondetails.FetchDetailQuestionsUseCase
-import com.android.mvcdandagger.screens.questionslist.FetchQuestionsUseCase
 
 class ActivityCompositionRoot(
     private val activity: AppCompatActivity,
     private val appCompositionRoot: AppCompositionRoot
 ) {
 
-    //todo 2 (next BaseActivity)
     val screenNavigator by lazy {
         ScreenNavigator(activity)
     }
 
-    val layoutInflater get() = LayoutInflater.from(activity)
+    val layoutInflater: LayoutInflater get() = LayoutInflater.from(activity)
     val fragmentManager get() = activity.supportFragmentManager
     val stackoverflowApi get() = appCompositionRoot.stackoverflowApi
 }
